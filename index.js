b215d6249d2e502e90bac9371f0e6ff07e4f8371
Stephanie Sadowski <stephaniesadowski@Stephanies-Air.home>
2022-07-01 23:07:15 -0400

    removed comments

--- index.js
+++ index.js
@@ -1,14 +1,3 @@
-/*Frontend - HTML/JS/CSS
-Work on scripts.js to pull data from Sample API
-Backend - Node.js / Express
-Create new project folder (created index.js)
-initalize npm (in terminal, npm init -y)
-Install deps (in terminal, npm i express && cors)
-get API to listen on port 5000
-create a json file with all coffees from Sample APIS
-Create / route so that front end is able to get Coffees
-Connect Front end getting Coffees from your local API.*/
-
 const express = require('express')
 const cors = require('cors')
 const restaurants = require('./restaurants.json')
